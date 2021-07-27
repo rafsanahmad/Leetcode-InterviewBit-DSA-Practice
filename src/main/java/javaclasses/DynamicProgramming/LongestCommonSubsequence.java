d98ef1696f31b35fe186256e57117e8c0f711823
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-07-28 01:45:51 +0600

    updates

---
+++ src/main/java/javaclasses/DynamicProgramming/LongestCommonSubsequence.java
@@ -0,0 +1,56 @@
+package javaclasses.DynamicProgramming;
+
+public class LongestCommonSubsequence {
+    //Leetcode 1143
+    /*Given two strings text1 and text2, return the length of their longest common subsequence.
+    If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters
+(can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+Example 1:
+Input: text1 = "abcde", text2 = "ace"
+Output: 3
+Explanation: The longest common subsequence is "ace" and its length is 3.
+
+Example 2:
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+
+Example 3:
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+ */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int max = 0;
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+                max = Math.max(dp[i][j], max);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String args[]) {
+        LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+        String str1 = "ABCDGHLQR";
+        String str2 = "AEDPHR";
+
+        int result = lcs.longestCommonSubsequence(str1, str2);
+        System.out.print(result);
+    }
+}
---
+++ src/main/java/javaclasses/DynamicProgramming/MaximumLengthOfRepeatedSubarray.java
@@ -0,0 +1,37 @@
+package javaclasses.DynamicProgramming;
+
+public class MaximumLengthOfRepeatedSubarray {
+    //Leetcode 718
+    /*Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears in both arrays.
+
+Example 1:
+Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+Output: 3
+Explanation: The repeated subarray with maximum length is [3,2,1].
+
+Example 2:
+Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+Output: 5*/
+
+    public int findLength(int[] nums1, int[] nums2) {
+        int max = 0;
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = Math.max(dp[i][j], max);
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        MaximumLengthOfRepeatedSubarray subarray = new MaximumLengthOfRepeatedSubarray();
+        int[] ar1 = {1, 2, 3, 2, 1};
+        int[] ar2 = {3, 2, 1, 4, 7};
+        System.out.println(subarray.findLength(ar1, ar2));
+    }
+
+}
