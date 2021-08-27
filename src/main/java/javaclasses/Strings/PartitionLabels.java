0361ad78f3fe64431f573d12285d352fdb4ea153
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-08-27 21:22:17 +0600

    updates

---
+++ src/main/java/javaclasses/Strings/PartitionLabels.java
@@ -0,0 +1,46 @@
+package javaclasses.Strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartitionLabels {
+    //Leetcode 763
+    /*You are given a string s. We want to partition the string into as many parts as possible so that each
+    letter appears in at most one part.
+
+Return a list of integers representing the size of these parts.
+
+Example 1:
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+
+Example 2:
+Input: s = "eccbbbbdec"
+Output: [10]*/
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> result = new ArrayList<>();
+        int[] last = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+        int j = 0;
+        int anchor = 0;
+        for (int i = 0; i < s.length(); i++) {
+            j = Math.max(j, last[s.charAt(i) - 'a']);
+            if (i == j) {
+                result.add(i - anchor + 1);
+                anchor = i + 1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        PartitionLabels labels = new PartitionLabels();
+        System.out.println(labels.partitionLabels("eccbbbbdec"));
+    }
+}
---
+++ src/main/java/javaclasses/Tree/VerifyPreOrderSerialization.java
@@ -0,0 +1,49 @@
+package javaclasses.Tree;
+
+public class VerifyPreOrderSerialization {
+    //Leetcode 331
+    //res/pre-tree.jpeg
+    /*One way to serialize a binary tree is to use preorder traversal. When we encounter a non-null node,
+    we record the node's value. If it is a null node, we record using a sentinel value such as '#'.
+
+For example, the above binary tree can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#",
+where '#' represents a null node.
+
+Given a string of comma-separated values preorder, return true if it is a correct preorder traversal
+serialization of a binary tree.
+
+It is guaranteed that each comma-separated value in the string must be either an integer or a character
+'#' representing null pointer.
+
+You may assume that the input format is always valid.
+
+For example, it could never contain two consecutive commas, such as "1,,3".
+Note: You are not allowed to reconstruct the tree.
+
+Example 1:
+Input: preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#"
+Output: true
+
+Example 2:
+Input: preorder = "1,#"
+Output: false
+
+Example 3:
+Input: preorder = "9,#,#,1"
+Output: false*/
+
+    public boolean isValidSerialization(String preorder) {
+        String[] nodes = preorder.split(",");
+        int nodeCount = 1;
+        for (String node : nodes) {
+            nodeCount--;
+            if (nodeCount < 0) {
+                return false;
+            }
+            if (!node.equals("#")) {
+                nodeCount += 2;
+            }
+        }
+        return nodeCount == 0;
+    }
+}
