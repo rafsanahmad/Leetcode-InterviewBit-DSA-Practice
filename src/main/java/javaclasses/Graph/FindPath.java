52f48237969faca97084a9e5dd0b5aa72c45eca9
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-09-15 02:00:12 +0600

    updates

---
+++ src/main/java/javaclasses/Graph/FindPath.java
@@ -0,0 +1,92 @@
+package javaclasses.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindPath {
+
+    /*Given a grid and a word, write a function that returns the location of the word in the grid as a list
+    of coordinates. If there are multiple matches, return any one.
+
+grid1 = [
+	['c', 'c', 'c', 'a', 'r', 's'],
+	['c', 'c', 'i', 't', 'n', 'b'],
+	['a', 'c', 'n', 'n', 't', 'i'],
+	['t', 'c', 'i', 'i', 'p', 't']
+]
+
+word1_1 = "catnip"
+find_word_location(grid1, word1_1)-> [ (0, 2), (0, 3), (1, 3), (2, 3), (3, 3), (3, 4) ]
+
+word1_2 = "cccc"
+find_word_location(grid1, word1_2)-> [(0, 1), (1, 1), (2, 1), (3, 1)]
+OR [(0, 0), (1, 0), (1, 1), (2, 1)]
+OR [(0, 0), (0, 1), (1, 1), (2, 1)]
+OR [(1, 0), (1, 1), (2, 1), (3, 1)]
+
+
+grid2 = [
+    ['c', 'p', 'a', 'n', 't', 's'],
+    ['a', 'b', 'i', 't', 'a', 'b'],
+    ['t', 'f', 'n', 'n', 'c', 'i'],
+    ['x', 's', 'c', 'a', 't', 'n'],
+    ['x', 's', 'd', 'd', 'e', 'a'],
+    ['s', 'q', 'w', 'x', 's', 'p']
+]
+
+
+word2 = "catnap"
+find_word_location(grid2, word2)-> [ (3, 2), (3, 3), (3, 4), (3, 5), (4, 5), (5, 5) ]
+*/
+    List<ArrayList> output = new ArrayList<>();
+
+    private void search(char[][] grid1, String word) {
+        boolean[][] visited = new boolean[grid1.length][grid1[0].length];
+
+        for (int i = 0; i < grid1.length; i++) {
+            for (int j = 0; j < grid1.length; j++) {
+                if (grid1[i][j] == word.charAt(0)) {
+                    dfs(grid1, i, j, new ArrayList<String>(), word, 0, visited);
+                }
+            }
+        }
+
+        if (output.size() == 0) {
+            System.out.print("None");
+        } else {
+            System.out.print(output.toString());
+        }
+    }
+
+
+    private void dfs(char[][] grid1, int i, int j, ArrayList<String> indexTracker, String word, int wordIndex, boolean[][] visited) {
+
+        if (i < 0 || i >= grid1.length || j < 0 || j >= grid1[i].length || grid1[i][j] != word.charAt(wordIndex) || visited[i][j] == true) {
+            return;
+        }
+
+        indexTracker.add("(" + i + ", " + j + ")");
+        visited[i][j] = true;
+        if (wordIndex == word.length() - 1) {
+            System.out.println("Found word");
+            output.add(new ArrayList<String>(indexTracker));
+            return;
+        }
+
+        dfs(grid1, i + 1, j, indexTracker, word, wordIndex + 1, visited);
+        dfs(grid1, i, j + 1, indexTracker, word, wordIndex + 1, visited);
+
+    }
+
+    public static void main(String[] args) {
+        char[][] chArray = {
+                {'c', 'p', 'a', 'n', 't', 's'},
+                {'a', 'b', 'i', 't', 'a', 'b'},
+                {'t', 'f', 'n', 'n', 'c', 'i'},
+                {'x', 's', 'c', 'a', 't', 'n'},
+                {'x', 's', 'd', 'd', 'e', 'a'},
+                {'s', 'q', 'w', 'x', 's', 'p'}
+        };
+        new FindPath().search(chArray, "catnap");
+    }
+}
---
+++ src/main/java/javaclasses/Utility/FlatToHierarchicalTree.java
@@ -0,0 +1,114 @@
+package javaclasses.Utility;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+public class FlatToHierarchicalTree<T> {
+
+    private T head;
+
+    private ArrayList<FlatToHierarchicalTree<T>> leafs = new ArrayList<>();
+
+    private FlatToHierarchicalTree<T> parent = null;
+
+    private HashMap<T, FlatToHierarchicalTree<T>> locate = new HashMap<>();
+
+    public FlatToHierarchicalTree(T head) {
+        this.head = head;
+        locate.put(head, this);
+    }
+
+    public void addLeaf(T root, T leaf) {
+        if (locate.containsKey(root)) {
+            locate.get(root).addLeaf(leaf);
+        } else {
+            addLeaf(root).addLeaf(leaf);
+        }
+    }
+
+    public FlatToHierarchicalTree<T> addLeaf(T leaf) {
+        FlatToHierarchicalTree<T> t = new FlatToHierarchicalTree<T>(leaf);
+        leafs.add(t);
+        t.parent = this;
+        t.locate = this.locate;
+        locate.put(leaf, t);
+        return t;
+    }
+
+    public FlatToHierarchicalTree<T> setAsParent(T parentRoot) {
+        FlatToHierarchicalTree<T> t = new FlatToHierarchicalTree<T>(parentRoot);
+        t.leafs.add(this);
+        this.parent = t;
+        t.locate = this.locate;
+        t.locate.put(head, this);
+        t.locate.put(parentRoot, t);
+        return t;
+    }
+
+    public T getHead() {
+        return head;
+    }
+
+    public FlatToHierarchicalTree<T> getTree(T element) {
+        return locate.get(element);
+    }
+
+    public FlatToHierarchicalTree<T> getParent() {
+        return parent;
+    }
+
+    public Collection<T> getSuccessors(T root) {
+        Collection<T> successors = new ArrayList<T>();
+        FlatToHierarchicalTree<T> tree = getTree(root);
+        if (null != tree) {
+            for (FlatToHierarchicalTree<T> leaf : tree.leafs) {
+                successors.add(leaf.head);
+            }
+        }
+        return successors;
+    }
+
+    public Collection<FlatToHierarchicalTree<T>> getSubTrees() {
+        return leafs;
+    }
+
+    public static <T> Collection<T> getSuccessors(T of, Collection<FlatToHierarchicalTree<T>> in) {
+        for (FlatToHierarchicalTree<T> tree : in) {
+            if (tree.locate.containsKey(of)) {
+                return tree.getSuccessors(of);
+            }
+        }
+        return new ArrayList<T>();
+    }
+
+    @Override
+    public String toString() {
+        return printTree(0);
+    }
+
+    private static final int indent = 2;
+
+    private String printTree(int increment) {
+        String s = "";
+        String inc = "";
+        for (int i = 0; i < increment; ++i) {
+            inc = inc + " ";
+        }
+        s = inc + head;
+        for (FlatToHierarchicalTree<T> child : leafs) {
+            s += "\n" + child.printTree(increment + indent);
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        FlatToHierarchicalTree<Integer> tree = new FlatToHierarchicalTree<>(1);
+        tree.addLeaf(1, 2);
+        tree.addLeaf(2, 3);
+        tree.addLeaf(3, 4);
+        tree.addLeaf(3, 5);
+        tree.addLeaf(2, 6);
+        System.out.println(tree.printTree(2));
+    }
+}
