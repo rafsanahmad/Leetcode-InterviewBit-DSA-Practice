095d90bc22b64ff78d0fca807a5051139ca93124
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-12-06 16:48:14 +0600

    updates

--- src/main/java/javaclasses/Graph/BFS_Shortest_path.java
+++ src/main/java/javaclasses/Graph/BFS_Shortest_path.java
@@ -1,5 +1,5 @@
 /*
- * *
+ * * BFS Shortest path.java
  *  * Created by Rafsan Ahmad on 10/25/21, 9:49 PM
  *  * Copyright (c) 2021 . All rights reserved.
  *
@@ -15,7 +15,62 @@ import java.util.Stack;
 
 //https://www.hackerrank.com/challenges/ctci-bfs-shortest-reach/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=graphs
 public class BFS_Shortest_path {
+    /*Consider an undirected graph consisting of n nodes where each node is labeled from 1 to n and the edge between
+    any two nodes is always of length 6. We define node s to be the starting position for a BFS.
+    Given a graph, determine the distances from the start node to each of its descendants and return the list in node
+    number order, ascending. If a node is disconnected, it's distance should be -1.
 
+For example, there are n=6 nodes in the graph with a starting node s. The list of edges = [[1,2],[1,5],[2,3],[3,4]]
+, and each has a weight of 6.
+
+           1          6
+         /   \
+        5     2
+              \
+               3
+                \
+                 4
+
+  Starting from node 1 and creating a list of distances, for nodes 2 through 6 we have [6,12,18,6,-1]
+
+
+  Function Description
+
+Define a Graph class with the required methods to return a list of distances.
+
+Input Format
+
+The first line contains an integer, q , the number of queries.
+
+Each of the following q sets of lines is as follows:
+
+The first line contains two space-separated integers, n and m, the number of nodes and the number of edges.
+Each of the next m lines contains two space-separated integers, u and v, describing an edge connecting node u to node v.
+The last line contains a single integer, s , the index of the starting node.
+Constraints
+
+Output Format
+
+For each of the q queries, print a single line of n-1 space-separated integers denoting the shortest distances to each
+of the n-1 other nodes from starting position s.
+These distances should be listed sequentially by node number (i.e. 1,2,..n-1), but should not include node s.
+If some node is unreachable from s, print -1 as the distance to that node.
+
+Sample Input
+
+2
+4 2
+1 2
+1 3
+1
+3 1
+2 3
+2
+Sample Output
+
+6 6 -1
+-1 6
+*/
     public static class Graph {
 
         private ArrayList<Integer>[] adj;
