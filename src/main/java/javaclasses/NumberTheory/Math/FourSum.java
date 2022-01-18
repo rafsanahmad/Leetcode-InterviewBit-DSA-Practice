7395d364a97e186d7de3f487f542f01be20bf51c
Rafsan Ahmad <rafsanahmad111@gmail.com>
2022-01-19 00:14:13 +0600

    updates

--- src/main/java/javaclasses/NumberTheory/Math/FourSum.java
+++ src/main/java/javaclasses/NumberTheory/Math/FourSum.java
@@ -14,24 +14,22 @@ import java.util.List;
 public class FourSum {
     //Leetcode 18
     /*
-    * Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+    * Given an array nums of n integers, return an array of all the unique quadruplets
+    * [nums[a], nums[b], nums[c], nums[d]] such that:
 0 <= a, b, c, d < n
 a, b, c, and d are distinct.
 nums[a] + nums[b] + nums[c] + nums[d] == target
 You may return the answer in any order.
 
 Example 1:
-
 Input: nums = [1,0,-1,0,-2,2], target = 0
 Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
-Example 2:
 
+Example 2:
 Input: nums = [2,2,2,2,2], target = 8
 Output: [[2,2,2,2]]
-*
-* Time complexity = O(n3)
  */
-
+    //Time complexity = O(n^3)
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null || nums.length == 0) {
@@ -73,6 +71,52 @@ Output: [[2,2,2,2]]
         return result;
     }
 
+    public List<List<Integer>> fourSumGeneralized(int[] nums, int target) {
+        Arrays.sort(nums);
+        //K = 4
+        return kSum(nums, 0, 4, target);
+    }
+
+    public List<List<Integer>> kSum(int[] nums, int start, int k, int target) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (k == 2) {
+            //two pointers from left and right
+            int left = start, right = len - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    List<Integer> pair = new ArrayList<>();
+                    pair.add(nums[left]);
+                    pair.add(nums[right]);
+                    res.add(pair);
+                    left++;
+                    right--;
+
+                    //Ignore duplicate
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                } else if (sum < target) {
+                    //Move left
+                    left++;
+                } else {
+                    //Move right
+                    right--;
+                }
+            }
+        } else {
+            for (int i = start; i < len - (k - 1); i++) {
+                if (i > start && nums[i] == nums[i - 1]) continue;
+                List<List<Integer>> temp = kSum(nums, i + 1, k - 1, target - nums[i]);
+                for (List<Integer> t : temp) {
+                    t.add(0, nums[i]);
+                }
+                res.addAll(temp);
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] ar = {1, 0, -1, 0, -2, 2};
         FourSum sum = new FourSum();
@@ -81,6 +125,16 @@ Output: [[2,2,2,2]]
             System.out.print("[");
             for (int j = 0; j < result.get(i).size(); j++) {
                 System.out.print(result.get(i).get(j) + " ");
+            }
+            System.out.print("]");
+        }
+        System.out.println();
+        
+        List<List<Integer>> result2 = sum.fourSumGeneralized(ar, 0);
+        for (int i = 0; i < result2.size(); i++) {
+            System.out.print("[");
+            for (int j = 0; j < result2.get(i).size(); j++) {
+                System.out.print(result2.get(i).get(j) + " ");
             }
             System.out.print("]");
         }
