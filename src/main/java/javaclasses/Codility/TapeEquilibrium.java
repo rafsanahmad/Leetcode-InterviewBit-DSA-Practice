c10714669c8a75a54fb8d30781131084fc2c97da
Rafsan Ahmad <rafsanahmad111@gmail.com>
2022-01-23 02:25:03 +0600

    updates

---
+++ src/main/java/javaclasses/Codility/TapeEquilibrium.java
@@ -0,0 +1,103 @@
+/*
+ * *
+ *  * Tape Equilibrium.java
+ *  * Created by Rafsan Ahmad on 1/23/22, 12:45 AM
+ *  * Copyright (c) 2022 . All rights reserved.
+ *
+ */
+
+package javaclasses.Codility;
+
+public class TapeEquilibrium {
+    /*A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+
+Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and
+A[P], A[P + 1], ..., A[N − 1].
+
+The difference between the two parts is the value of:
+|(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+
+In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+
+For example, consider array A such that:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+We can split this tape in four places:
+
+P = 1, difference = |3 − 10| = 7
+P = 2, difference = |4 − 9| = 5
+P = 3, difference = |6 − 7| = 1
+P = 4, difference = |10 − 3| = 7
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+
+For example, given:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+the function should return 1, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [2..100,000];
+each element of array A is an integer within the range [−1,000..1,000].*/
+
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int total = 0;
+
+        for (int i = 0; i < A.length; i++)
+            total += A[i];
+
+        int diff = Integer.MAX_VALUE;
+        int prev = 0;
+        int next = total;
+
+        for (int p = 1; p < A.length; p++) {
+            prev += A[p - 1];
+            next = total - prev;
+            diff = Math.min(diff, Math.abs(prev - next));
+        }
+
+        return diff;
+    }
+
+    public int solutionUsingPrefixSum(int[] A) {
+        int total = 0;
+        int len = A.length;
+        int[] prefixSum = new int[len + 1];
+        int minResult = Integer.MAX_VALUE;
+        for (int i = 0; i < len; i++) {
+            total += A[i];
+            if (i == 0) prefixSum[i] = A[i];
+            else prefixSum[i] = prefixSum[i - 1] + A[i];
+        }
+        for (int i = 0; i < len - 1; i++) {
+            int d = total - prefixSum[i];
+            int diff = Math.abs(prefixSum[i] - d);
+            minResult = Math.min(diff, minResult);
+        }
+        return minResult;
+    }
+
+    public static void main(String[] args) {
+        TapeEquilibrium equilibrium = new TapeEquilibrium();
+        int[] arr = {3, 1, 2, 4, 3};
+        int[] arr2 = {-1000, 1000};
+        System.out.println(equilibrium.solution(arr));
+        System.out.println(equilibrium.solution(arr2));
+
+        System.out.println(equilibrium.solutionUsingPrefixSum(arr));
+        System.out.println(equilibrium.solutionUsingPrefixSum(arr2));
+    }
+}
