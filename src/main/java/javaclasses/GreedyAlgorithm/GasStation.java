37744dfa4c63f4a63906f39e059003c15d9ebe7a
Rafsan Ahmad <rafsanahmad111@gmail.com>
2022-01-29 21:41:07 +0600

    updates

--- src/main/java/javaclasses/GreedyAlgorithm/GasStation.java
+++ src/main/java/javaclasses/GreedyAlgorithm/GasStation.java
@@ -8,6 +8,7 @@
 package javaclasses.GreedyAlgorithm;
 
 public class GasStation {
+    //Leetcode 134
     //https://www.interviewbit.com/problems/gas-station/
     /*Given two integer arrays A and B of size N.
 
@@ -44,9 +45,23 @@ Output 1:
         If you start from index 1, you can fill in A[1] = 2 amount of gas. Now your tank has 2 units of gas.
         You need B[1] = 1 gas to get to station 0. So, you travel to station 0 and still have 1 unit of gas
         left over. You fill in A[0] = 1 unit of additional gas, making your current gas = 2.
-        It costs you B[0] = 2 to get to station 1, which you do and complete the circuit.*/
+        It costs you B[0] = 2 to get to station 1, which you do and complete the circuit.
 
-    public int canCompleteCircuit(final int[] gas, final int[] dist) {
+
+Example 2:
+Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+Output: 3
+
+Explanation:
+Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 4. Your tank = 4 - 1 + 5 = 8
+Travel to station 0. Your tank = 8 - 2 + 1 = 7
+Travel to station 1. Your tank = 7 - 3 + 2 = 6
+Travel to station 2. Your tank = 6 - 4 + 3 = 5
+Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+Therefore, return 3 as the starting index.*/
+
+    public int canCompleteCircuit(final int[] gas, final int[] cost) {
         int start = 0, totalGasUsed = 0, totalGasFound = 0;
 
         int gasRequiredBefore = 0;
@@ -55,7 +70,7 @@ Output 1:
 
 
         for (int i = 0; i < gas.length; i++) {
-            totalGasUsed += dist[i];
+            totalGasUsed += cost[i];
             totalGasFound += gas[i];
             if (totalGasFound - totalGasUsed < 0) {
                 gasRequiredBefore += totalGasUsed;
@@ -72,7 +87,7 @@ Output 1:
     }
 
     //using two pointer
-    public int canCompleteCircuitSimple(final int[] gas, final int[] dist) {
+    public int canCompleteCircuitSimple(final int[] gas, final int[] cost) {
         // TODO: check A==null || B==null || A.size()!=B.size()
         int n = gas.length;
         int start = 0;
@@ -81,8 +96,8 @@ Output 1:
         int curr_gas = 0;
 
         while (end < n) {
-            curr_gas += gas[end] - dist[end];
-            total += gas[end] - dist[end];
+            curr_gas += gas[end] - cost[end];
+            total += gas[end] - cost[end];
             if (curr_gas >= 0) {
                 end++;
             } else {
@@ -99,8 +114,8 @@ Output 1:
 
     public static void main(String[] args) {
         int[] gas = {1, 2, 3, 4, 5};
-        int[] dist = {3, 4, 5, 1, 2};
+        int[] cost = {3, 4, 5, 1, 2};
         GasStation station = new GasStation();
-        System.out.println(station.canCompleteCircuitSimple(gas, dist));
+        System.out.println(station.canCompleteCircuitSimple(gas, cost));
     }
 }
