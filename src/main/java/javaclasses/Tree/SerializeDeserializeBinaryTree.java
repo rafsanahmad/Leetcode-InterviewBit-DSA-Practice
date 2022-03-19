63816436fcdc0780c7c8f117510c3286b5e906b7
Rafsan Ahmad <rafsanahmad111@gmail.com>
2022-03-20 01:53:18 +0600

    updates

--- src/main/java/javaclasses/Tree/SerializeDeserializeBinaryTree.java
+++ src/main/java/javaclasses/Tree/SerializeDeserializeBinaryTree.java
@@ -12,58 +12,28 @@ import java.util.List;
 import java.util.Stack;
 
 public class SerializeDeserializeBinaryTree {
+    //Leetcode 297
+    /*Serialization is the process of converting a data structure or object into a sequence of bits so that it can
+    be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later
+    in the same or another computer environment.
 
-    /*Serialization is to store tree in a file so that it can be later restored.
-    The structure of tree must be maintained. Deserialization is reading tree back from file.
+Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/
+deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and '
+this string can be deserialized to the original tree structure.
 
-    Let the marker for NULL pointers be '-1'
-Input:
-     12
-    /
-  13
-Output: 12 13 -1 -1 -1
+         1
+       /   \
+      2     3
+          /  \
+         4    5
 
-Input:
-      20
-    /   \
-   8     22
-Output: 20 8 -1 -1 22 -1 -1
-
-Input:
-         20
-       /
-      8
-     / \
-    4  12
-      /  \
-     10  14
-Output: 20 8 4 -1 -1 12 10 -1 -1 14 -1 -1 -1
-
-Input:
-          20
-         /
-        8
-      /
-    10
-    /
-   5
-Output: 20 8 10 5 -1 -1 -1 -1 -1
-
-Input:
-          20
-            \
-             8
-              \
-               10
-                 \
-                  5
-Output: 20 -1 8 -1 10 -1 5 -1 -1
+Input: root = [1,2,3,null,null,4,5]
+Output: [1,2,3,null,null,4,5]
     */
 
-    TreeNode root;
-
+    //Approach: Using Preorder Traversal
     // Encodes a tree to a single string.
-    public static String serialize(TreeNode root) {
+    public String serialize(TreeNode root) {
         if (root == null) {
             return null;
         }
@@ -80,67 +50,60 @@ Output: 20 -1 8 -1 10 -1 5 -1 -1
                 // Else, store current node and recur for
                 // its children
                 l.add("" + t.val);
-                s.push(t.right);
+                s.push(t.right);  //Key point to note - to reverse the order while serializing
                 s.push(t.left);
             }
         }
         return String.join(",", l);
     }
 
-    static int t;
+    int index = 0;
 
     // Decodes your encoded data to tree.
-    public static TreeNode deserialize(String data) {
+    public TreeNode deserialize(String data) {
         if (data == null)
             return null;
-        t = 0;
         String[] arr = data.split(",");
         return helper(arr);
     }
 
-    public static TreeNode helper(String[] arr) {
-        if (arr[t].equals("#"))
+    public TreeNode helper(String[] arr) {
+        if (arr[index].equals("#"))
             return null;
         // create node with this item and recur for children
-        TreeNode root = new TreeNode(Integer.parseInt(arr[t]));
-        t++;
+        TreeNode root = new TreeNode(Integer.parseInt(arr[index]));
+        index++;
         root.left = helper(arr);
-        t++;
+        index++;
         root.right = helper(arr);
         return root;
     }
 
-    // A simple inorder traversal used for testing the
-    // constructed tree
-    static void inorder(TreeNode root) {
+    // A simple inorder traversal used for testing the constructed tree
+    public void preOrder(TreeNode root) {
         if (root != null) {
-            inorder(root.left);
             System.out.print(root.val + " ");
-            inorder(root.right);
+            preOrder(root.left);
+            preOrder(root.right);
         }
     }
 
-    /* Driver program to test above functions*/
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         // Let us construct a tree shown in the above figure
-        SerializeDeserializeBinaryTree tree = new SerializeDeserializeBinaryTree();
-        tree.root = new TreeNode(20);
-        tree.root.left = new TreeNode(8);
-        tree.root.right = new TreeNode(22);
-        tree.root.left.left = new TreeNode(4);
-        tree.root.left.right = new TreeNode(12);
-        tree.root.left.right.left = new TreeNode(10);
-        tree.root.left.right.right = new TreeNode(14);
+        SerializeDeserializeBinaryTree bst = new SerializeDeserializeBinaryTree();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(5);
 
-        String serialized = serialize(tree.root);
+        String serialized = bst.serialize(root);
         System.out.println("Serialized view of the tree:");
         System.out.println(serialized);
-        System.out.println();
 
         // Let us deserialize the stored tree into root1
-        TreeNode t = deserialize(serialized);
-
-        System.out.println("Inorder Traversal of the tree constructed from serialized String:");
-        inorder(t);
+        TreeNode t = bst.deserialize(serialized);
+        System.out.println("Preorder Traversal of the tree constructed from serialized String:");
+        bst.preOrder(t);
     }
 }
