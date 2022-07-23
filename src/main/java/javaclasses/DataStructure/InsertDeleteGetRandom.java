2a8d42dfbf2098dfe5632b9875c01feb02d29ccf
Rafsan Ahmad <rafsanahmad111@gmail.com>
2022-07-23 16:31:18 +0600

    updates

---
+++ src/main/java/javaclasses/DataStructure/InsertDeleteGetRandom.java
@@ -0,0 +1,121 @@
+/*
+ * *
+ *  * InsertDeleteGetRandom.java
+ *  * Created by Rafsan Ahmad on 12/10/21, 3:48 AM
+ *  * Copyright (c) 2022 . All rights reserved.
+ *
+ */
+
+package javaclasses.DataStructure;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class InsertDeleteGetRandom {
+
+    //Leetcode 380
+    /*Implement the RandomizedSet class:
+
+RandomizedSet() Initializes the RandomizedSet object.
+bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present,
+false otherwise.
+bool remove(int val) Removes an item val from the set if present. Returns true if the item was present,
+false otherwise.
+int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one
+element exists when this method is called).
+Each element must have the same probability of being returned.
+
+Example 1:
+
+Input
+["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+[[], [1], [2], [2], [], [1], [2], []]
+Output
+[null, true, false, true, 2, true, false, 2]
+
+Explanation
+RandomizedSet randomizedSet = new RandomizedSet();
+randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
+randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
+randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+randomizedSet.insert(2); // 2 was already in the set, so return false.
+randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
+
+The operations should run in O(1) time
+*/
+
+    static class RandomizedSet {
+
+        /**
+         * Initialize your data structure here.
+         */
+        private List<Integer> list;
+        private HashMap<Integer, Integer> map;
+        private Random random;
+
+        public RandomizedSet() {
+            list = new ArrayList<>();
+            map = new HashMap<>();
+            random = new Random();
+        }
+
+        /**
+         * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+         */
+        public boolean insert(int val) {
+            if (map.containsKey(val)) {
+                return false;
+            }
+            map.put(val, list.size());
+            list.add(val);
+            return true;
+        }
+
+        /**
+         * Removes a value from the set. Returns true if the set contained the specified element.
+         */
+        public boolean remove(int val) {
+            if (!map.containsKey(val)) {
+                return false;
+            }
+            //swap last & removed element & remove the last element
+            int size = list.size() - 1;
+            int lastElement = list.get(size);
+            int indexToRemove = map.get(val);
+            list.set(indexToRemove, lastElement);
+            map.put(lastElement, indexToRemove);
+            map.remove(val);
+            list.remove(size);
+            return true;
+        }
+
+        /**
+         * Get a random element from the set.
+         */
+        public int getRandom() {
+            return list.get(random.nextInt(list.size()));
+        }
+    }
+
+    /**
+     * Your RandomizedSet object will be instantiated and called as such:
+     * RandomizedSet obj = new RandomizedSet();
+     * boolean param_1 = obj.insert(val);
+     * boolean param_2 = obj.remove(val);
+     * int param_3 = obj.getRandom();
+     */
+    public static void main(String[] args) {
+        RandomizedSet randomizedSet = new RandomizedSet();
+        System.out.println(randomizedSet.insert(1));
+        System.out.println(randomizedSet.remove(2));
+        System.out.println(randomizedSet.insert(2));
+        System.out.println(randomizedSet.getRandom());
+        System.out.println(randomizedSet.remove(1));
+        System.out.println(randomizedSet.insert(2));
+        System.out.println(randomizedSet.getRandom());
+    }
+}
