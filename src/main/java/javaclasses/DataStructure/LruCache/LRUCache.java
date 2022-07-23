9fd7900678f96b1df62c48eed54c29ebaf59595b
Rafsan Ahmad <rafsanahmad111@gmail.com>
2022-07-23 16:31:00 +0600

    updates

---
+++ src/main/java/javaclasses/DataStructure/LruCache/LRUCache.java
@@ -0,0 +1,126 @@
+/*
+ * * LRU Cache.java
+ *  * Created by Rafsan Ahmad on 10/25/21, 9:49 PM
+ *  * Copyright (c) 2021 . All rights reserved.
+ *
+ */
+
+package javaclasses.DataStructure.LruCache;
+
+import java.util.HashMap;
+
+/*Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+        Implement the LRUCache class:
+
+        LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+        int get(int key) Return the value of the key if the key exists, otherwise return -1.
+        void put(int key, int value) Update the value of the key if the key exists. Otherwise,
+        add the key-value pair to the cache.
+        If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+        Follow up:
+        Could you do get and put in O(1) time complexity?
+
+        Example 1:
+
+        Input
+        ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+        [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+        Output
+        [null, null, null, 1, null, -1, null, -1, 3, 4]
+
+        Explanation
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1); // cache is {1=1}
+        lRUCache.put(2, 2); // cache is {1=1, 2=2}
+        lRUCache.get(1);    // return 1
+        lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        lRUCache.get(2);    // returns -1 (not found)
+        lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+        lRUCache.get(1);    // return -1 (not found)
+        lRUCache.get(3);    // return 3
+        lRUCache.get(4);    // return 4*/
+
+//https://leetcode.com/problems/lru-cache/
+public class LRUCache {
+    Node head;
+    Node tail;
+
+    HashMap<Integer, Node> map = null;
+    int cap = 0;
+
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+        this.map = new HashMap<>();
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node t = map.get(key);
+            t.value = value;
+            //move to tail
+            removeNode(t);
+            insertNode(t);
+        } else {
+            if (map.size() >= cap) {
+                //delete head
+                map.remove(head.key);
+                removeNode(head);
+            }
+            //add to tail
+            Node node = new Node(key, value);
+            insertNode(node);
+            map.put(key, node);
+        }
+    }
+
+    public int get(int key) {
+        if (map.get(key) == null) {
+            return -1;
+        }
+        //mode to tail
+        Node t = map.get(key);
+        removeNode(t);
+        insertNode(t);
+        return t.value;
+    }
+
+    public void removeNode(Node n) {
+        if (n.prev != null) {
+            n.prev.next = n.next;
+        } else {
+            head = n.next;
+        }
+
+        if (n.next != null) {
+            n.next.prev = n.prev;
+        } else {
+            tail = n.prev;
+        }
+    }
+
+    public void insertNode(Node n) {
+        if (tail != null) {
+            tail.next = n;
+        }
+        n.prev = tail;
+        n.next = null;
+        tail = n;
+        if (head == null) {
+            head = tail;
+        }
+    }
+
+    public static void main(String[] args) {
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1); // cache is {1=1}
+        lRUCache.put(2, 2); // cache is {1=1, 2=2}
+        System.out.println(lRUCache.get(1));    // return 1
+        lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        System.out.println(lRUCache.get(2));    // returns -1 (not found)
+        lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+        System.out.println(lRUCache.get(1));    // return -1 (not found)
+        System.out.println(lRUCache.get(3));    // return 3
+        System.out.println(lRUCache.get(4));    // return 4*/
+    }
+}
---
+++ src/main/java/javaclasses/DataStructure/LruCache/Node.java
@@ -0,0 +1,20 @@
+/*
+ * *
+ *  * Created by Rafsan Ahmad on 10/25/21, 9:49 PM
+ *  * Copyright (c) 2021 . All rights reserved.
+ *
+ */
+
+package javaclasses.DataStructure.LruCache;
+
+public class Node {
+    int key;
+    int value;
+    public Node prev;
+    public Node next;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
