4ffe88b5b8fac71b7e39f72ee4b95be4bf98c2ac
Rafsan Ahmad <rafsanahmad111@gmail.com>
2022-12-17 20:27:34 +0600

    updates

--- src/main/java/javaclasses/DynamicProgramming/LongestCommonSubsequence.java
+++ src/main/java/javaclasses/DynamicProgramming/LongestCommonSubsequence.java
@@ -52,12 +52,78 @@ Explanation: There is no such common subsequence, so the result is 0.
         return max;
     }
 
+    //Printing Longest Common Subsequence
+    /*Given two sequences, print the longest subsequence present in both of them.
+
+Examples:
+LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+LCS for input Sequences “AGGTAB” and “GXTXAYB” is “GTAB” of length 4.
+
+Approach:
+1. Construct L[m+1][n+1] using the steps discussed in previous post.
+2. The value L[m][n] contains length of LCS. Create a character array lcs[] of length equal to the length of lcs plus
+1 (one extra to store \0).
+3. Traverse the 2D array starting from L[m][n]. Do following for every cell L[i][j]
+4. If characters (in X and Y) corresponding to L[i][j] are same (Or X[i-1] == Y[j-1]), then include this character as
+part of LCS.
+Else compare values of L[i-1][j] and L[i][j-1] and go in direction of greater value.
+
+*/
+
+    public String longestCommonSubsequenceStr(String text1, String text2) {
+        int max = 0;
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+                max = Math.max(dp[i][j], max);
+            }
+        }
+
+        // Following code is used to print LCS
+        int index = dp[len1][len2];
+
+        // Create a character array to store the lcs string
+        char[] lcs = new char[index];
+
+        // Start from the right-most-bottom-most corner and one by one store characters in lcs[]
+        while (len1 > 0 && len2 > 0) {
+            // If current character in X[] and Y are same, then current character is part of LCS
+            if (text1.charAt(len1 - 1) == text2.charAt(len2 - 1)) {
+                // Put current character in result
+                lcs[index - 1] = text1.charAt(len1 - 1);
+
+                // reduce values of i, j and index
+                len1--;
+                len2--;
+                index--;
+            }
+
+            // If not same, then find the larger of two and go in the direction of larger value
+            else if (dp[len1 - 1][len2] > dp[len1][len2 - 1])
+                len1--;
+            else
+                len2--;
+        }
+
+        return new String(lcs);
+    }
+
     public static void main(String args[]) {
         LongestCommonSubsequence lcs = new LongestCommonSubsequence();
         String str1 = "ABCDGHLQR";
         String str2 = "AEDPHR";
 
         int result = lcs.longestCommonSubsequence(str1, str2);
-        System.out.print(result);
+        System.out.println(result);
+
+        System.out.println(lcs.longestCommonSubsequenceStr(str1, str2));
     }
 }
