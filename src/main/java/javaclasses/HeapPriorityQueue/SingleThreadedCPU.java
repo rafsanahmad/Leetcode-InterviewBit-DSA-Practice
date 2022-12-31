f60e08e2a2d40ce1553c5ad5a3355cdb9c9c5aab
Rafsan Ahmad <rafsanahmad111@gmail.com>
2022-12-31 17:40:13 +0600

    updates

---
+++ src/main/java/javaclasses/HeapPriorityQueue/SingleThreadedCPU.java
@@ -0,0 +1,136 @@
+/*
+ * *
+ *  * Single Threaded CPU.java
+ *  * Created by Rafsan Ahmad on 12/31/22, 5:27 PM
+ *  * Copyright (c) 2022 . All rights reserved.
+ *
+ */
+
+package javaclasses.HeapPriorityQueue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class SingleThreadedCPU {
+    //https://leetcode.com/problems/single-threaded-cpu/description/
+    /*You are given n tasks labeled from 0 to n - 1 represented by a 2D integer array tasks,
+    where tasks[i] = [enqueueTimei, processingTimei] means that the ith task will be available to process at enqueueTimei
+    and will take processingTimei to finish processing.
+
+You have a single-threaded CPU that can process at most one task at a time and will act in the following way:
+
+If the CPU is idle and there are no available tasks to process, the CPU remains idle.
+If the CPU is idle and there are available tasks, the CPU will choose the one with the shortest processing time.
+If multiple tasks have the same shortest processing time, it will choose the task with the smallest index.
+Once a task is started, the CPU will process the entire task without stopping.
+The CPU can finish a task then start a new one instantly.
+Return the order in which the CPU will process the tasks.
+
+
+
+Example 1:
+
+Input: tasks = [[1,2],[2,4],[3,2],[4,1]]
+Output: [0,2,3,1]
+Explanation: The events go as follows:
+- At time = 1, task 0 is available to process. Available tasks = {0}.
+- Also at time = 1, the idle CPU starts processing task 0. Available tasks = {}.
+- At time = 2, task 1 is available to process. Available tasks = {1}.
+- At time = 3, task 2 is available to process. Available tasks = {1, 2}.
+- Also at time = 3, the CPU finishes task 0 and starts processing task 2 as it is the shortest. Available tasks = {1}.
+- At time = 4, task 3 is available to process. Available tasks = {1, 3}.
+- At time = 5, the CPU finishes task 2 and starts processing task 3 as it is the shortest. Available tasks = {1}.
+- At time = 6, the CPU finishes task 3 and starts processing task 1. Available tasks = {}.
+- At time = 10, the CPU finishes task 1 and becomes idle.
+
+
+Example 2:
+
+Input: tasks = [[7,10],[7,12],[7,5],[7,4],[7,2]]
+Output: [4,3,2,0,1]
+Explanation: The events go as follows:
+- At time = 7, all the tasks become available. Available tasks = {0,1,2,3,4}.
+- Also at time = 7, the idle CPU starts processing task 4. Available tasks = {0,1,2,3}.
+- At time = 9, the CPU finishes task 4 and starts processing task 3. Available tasks = {0,1,2}.
+- At time = 13, the CPU finishes task 3 and starts processing task 2. Available tasks = {0,1}.
+- At time = 18, the CPU finishes task 2 and starts processing task 0. Available tasks = {1}.
+- At time = 28, the CPU finishes task 0 and starts processing task 1. Available tasks = {}.
+- At time = 40, the CPU finishes task 1 and becomes idle.*/
+
+    class Tasks {
+        int enqueueTime;
+        int processingTime;
+        int index;
+
+        Tasks(int i, int e, int p) {
+            this.index = i;
+            this.enqueueTime = e;
+            this.processingTime = p;
+        }
+    }
+
+    public int[] getOrder(int[][] tasks) {
+        int[] order = new int[tasks.length];
+        List<Tasks> tasksList = new ArrayList<>();
+        for (int i = 0; i < tasks.length; i++) {
+            tasksList.add(new Tasks(i, tasks[i][0], tasks[i][1]));
+        }
+
+        Collections.sort(tasksList, new Comparator<Tasks>() {
+            @Override
+            public int compare(Tasks t1, Tasks t2) {
+                return t1.enqueueTime - t2.enqueueTime;
+            }
+        });
+
+        PriorityQueue<Tasks> queue = new PriorityQueue<>(new Comparator<Tasks>() {
+            @Override
+            public int compare(Tasks t1, Tasks t2) {
+                if (t1.processingTime == t2.processingTime) {
+                    return t1.index - t2.index;
+                } else {
+                    return t1.processingTime - t2.processingTime;
+                }
+            }
+        });
+
+        long currTime = 0;
+        int taskIndex = 0;
+        int ansIndex = 0;
+
+        while (!queue.isEmpty() || taskIndex < tasks.length) {
+            if (queue.isEmpty() && currTime < tasksList.get(taskIndex).enqueueTime) {
+                currTime = tasksList.get(taskIndex).enqueueTime;
+            }
+
+            while (taskIndex < tasks.length && currTime >= tasksList.get(taskIndex).enqueueTime) {
+                queue.add(tasksList.get(taskIndex));
+                taskIndex++;
+            }
+
+            if (queue.peek() != null) {
+                Tasks completed = queue.peek();
+                int processTime = completed.processingTime;
+                int index = completed.index;
+                queue.remove();
+
+                // Complete this task and increment currTime.
+                currTime += processTime;
+                order[ansIndex++] = index;
+            }
+        }
+
+        return order;
+    }
+
+    public static void main(String[] args) {
+        SingleThreadedCPU cpu = new SingleThreadedCPU();
+        int[][] tasks = {{1, 2}, {2, 4}, {3, 2}, {4, 1}};
+
+        System.out.println(Arrays.toString(cpu.getOrder(tasks)));
+    }
+}
--- src/main/java/javaclasses/Utility/TestClass.java
+++ src/main/java/javaclasses/Utility/TestClass.java
@@ -16,6 +16,6 @@ public class TestClass {
     }
 
     public static void main(String[] args) {
-
+        TestClass testClass = new TestClass();
     }
 }
