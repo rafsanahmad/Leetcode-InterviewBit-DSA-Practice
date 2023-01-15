a5ecd1eb63c727b38ed3840e91c7773a7d7fbf05
Rafsan Ahmad <rafsanahmad111@gmail.com>
2023-01-16 01:00:08 +0600

    updates

--- src/main/java/javaclasses/Recursion/AllStringPermutation.java
+++ src/main/java/javaclasses/Recursion/AllStringPermutation.java
@@ -7,55 +7,84 @@
 
 package javaclasses.Recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class AllStringPermutation {
     //res/permutation_recursion.gif
-    /*Below are the permutations of string ABC.
-      ABC ACB BAC BCA CBA CAB*/
+    /*
+    Generate all permutations of string in lexicographically sorted order where repetitions of
+    character is possible in string.
 
+    Below are the permutations of string ABC.
 
-    /**
-     * permutation function
-     *
-     * @param str string to calculate permutation for
-     * @param start   starting index
-     * @param end   end index
+    ABC
+    ACB
+    BAC
+    BCA
+    CAB
+    CBA
      */
+
     /*Time Complexity: O(n*n!) Note that there are n! permutations and it requires O(n) time to print a permutation.
-    Auxiliary Space: O(r – l)
+    Auxiliary Space: O(n) - n = length of string
     */
-    private void permute(String str, int start, int end) {
-        if (start == end)
-            System.out.print(str + " ");
-        else {
-            for (int i = start; i <= end; i++) {
-                str = swap(str, start, i);
-                permute(str, start + 1, end);
-                str = swap(str, start, i);
+
+    public List<String> permute(char[] input) {
+        Map<Character, Integer> countMap = new TreeMap<>();
+        for (char ch : input) {
+            countMap.compute(ch, (key, val) -> {
+                if (val == null) {
+                    return 1;
+                } else {
+                    return val + 1;
+                }
+            });
+        }
+        char[] str = new char[countMap.size()];
+        int[] count = new int[countMap.size()];
+        int index = 0;
+        for (Map.Entry<Character, Integer> entry : countMap.entrySet()) {
+            str[index] = entry.getKey();
+            count[index] = entry.getValue();
+            index++;
+        }
+
+        List<String> resultList = new ArrayList<>();
+        char[] result = new char[input.length];
+        permuteUtil(str, count, result, 0, resultList);
+        return resultList;
+    }
+
+    public void permuteUtil(char[] str, int[] count, char[] result, int level, List<String> resultList) {
+        if (level == result.length) {
+            resultList.add(new String(result));
+            return;
+        }
+
+        for (int i = 0; i < str.length; i++) {
+            if (count[i] == 0) {
+                continue;
             }
+            result[level] = str[i];
+            count[i]--;
+            permuteUtil(str, count, result, level + 1, resultList);
+            count[i]++;
         }
     }
 
-    /**
-     * Swap Characters at position
-     *
-     * @param a string value
-     * @param i position 1
-     * @param j position 2
-     * @return swapped string
-     */
-    public String swap(String a, int i, int j) {
-        char temp;
-        char[] charArray = a.toCharArray();
-        temp = charArray[i];
-        charArray[i] = charArray[j];
-        charArray[j] = temp;
-        return String.valueOf(charArray);
+    private void printArray(char[] input) {
+        for (char ch : input) {
+            System.out.print(ch);
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         String str = "ABC";
-        int n = str.length();
         AllStringPermutation permutation = new AllStringPermutation();
-        permutation.permute(str, 0, n - 1);
+        permutation.permute(str.toCharArray()).forEach(System.out::println);
     }
 }
