f5e7b3a0d6833031740b445fef3178076751c1b9
Rafsan Ahmad <rafsanahmad111@gmail.com>
2023-04-16 20:13:14 +0600

    updates

--- src/main/java/javaclasses/DynamicProgramming/LongestIncreasingSubsequence.java
+++ src/main/java/javaclasses/DynamicProgramming/LongestIncreasingSubsequence.java
@@ -78,12 +78,56 @@ Output: 1*/
         return list.size();
     }
 
+    /*Variant:
+    Given a word, write a program to calculate the smallest number of letters that must be removed in order
+    for the letters of the remaining word to be sorted in lexicographical order.
+ The resulting word need not appear in the dictionary of any particular language.
+
+For example given "banana" function should return 3 as we an remove character at 1st, 3rd, 6th to get the word
+"aan" which is sorted.
+
+Approach:
+In this program, we first initialize an integer array dp with all elements set to 1.
+The dp array will store the length of the longest increasing subsequence ending at each index of the given word.
+
+We then use nested loops to iterate over all pairs of indices (i, j) where i is greater than j. If the
+character at index i is greater than or equal to the character at index j, we update dp[i] to be the maximum
+of its current value and dp[j] + 1.
+
+After iterating over all pairs of indices, we find the maximum value in the dp array and return n - maxLength,
+where n is the length of the word. This gives us the smallest number of letters that must be removed in order
+for the remaining letters to be sorted in lexicographical order.
+    */
+
+    //Time Complexity: O(n^2)
+    public int minimumLettersToRemove(String word) {
+        int n = word.length();
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (word.charAt(i) >= word.charAt(j)) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+
+        return n - maxLength;
+    }
+
 
     public static void main(String[] args) {
         LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
         int[] arr = {10, 9, 2, 5, 3, 7, 101, 18};
         System.out.println(lis.lis(arr));
         System.out.println(lis.lisOptimized(arr));
+        System.out.println(lis.minimumLettersToRemove("banana"));
+        System.out.println(lis.minimumLettersToRemove("apple"));
     }
-
 }
